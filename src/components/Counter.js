4732ec81bb35651ee9f11557cea87baa97b1f707
abhinavyadav1806 <abhinav1806yadav@gmail.com>
2023-09-26 23:53:20 +0530

    Payload

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -15,6 +15,10 @@ const Counter = () => {
     dispatch({type: 'decrement'})
   }
 
+  const increaseHandler = () => {
+    dispatch({type: 'increase', amount: 5})
+  }
+
   const toggleCounterHandler = () => {};
 
   return (
@@ -24,6 +28,7 @@ const Counter = () => {
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
